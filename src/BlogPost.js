e6e2e3e3ae2a43eb873e30f0889414b7a94c2dff
jokale <kalemajoanna@gmail.com>
2020-04-29 15:31:03 +0100

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 // are we missing an import?
+import Comment from './Comment.js'
+
 
 export default class BlogPost extends Component {
   render() {
@@ -18,11 +20,23 @@ export default class BlogPost extends Component {
         Still I'll rise.<br/>
         -Maya Angelou<br/>
 
-        {/* (remove the comment ticks and their surrounding brackets)
+        
         <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        */}
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} />
+{/* 
+        <ColorBox color-box={color-box: [0.1]}/>
+        <ColorBox color-box={}/>
+        <ColorBox color-box={}/>
+        <ColorBox color-box={}/>
+        <ColorBox color-box={}/>
+        <ColorBox color-box={}/>
+        <ColorBox color-box={}/>
+        <ColorBox color-box={}/>
+        <ColorBox color-box={}/>
+        <ColorBox color-box={}/> */}
+
+        
       </div>
     );
   }
