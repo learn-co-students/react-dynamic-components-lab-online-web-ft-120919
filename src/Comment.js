50b670f847974ccc7086d3d9b5104368a67085da
Uchoosecode <awittyl44@gmail.com>
2020-06-23 13:23:33 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,19 @@
 //your code here
+import React, { Component } from 'react'; 
+
+export default class Comment extends Component {
+    render() {
+        commentText = () => {
+            return (
+            <div>
+                "poop"
+            </div>
+            )
+        }
+        return (
+        <div>
+            { this.prop.commentText() }
+        </div>
+        );
+    }
+}
